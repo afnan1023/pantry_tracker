e1e2d915646e7d4a51bcd23b8296dd1af537079e
Afnan Ediab <afnanediab20@hotmail.com>
2025-03-08 12:55:54 +0000

    Import a file

--- firebase.js
+++ firebase.js
@@ -7,6 +7,7 @@ import {
   createUserWithEmailAndPassword,
 } from "firebase/auth";
 import { getFirestore, doc, setDoc } from "firebase/firestore";
+import "dotenv/config"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
